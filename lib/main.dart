74070e6c5c9dd2dc61563d8400a2af4bd7b21dd2
phatcharaphon23 <phatcharaphon.h@ku.th>
2022-02-13 00:11:13 +0700

    555

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:test8/screen/display.dart';
+import 'package:test8/screen/formscreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -28,10 +33,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          body: TabBarView(children: []),
-          bottomNavigationBar: TabBar(tabs: [],),
-        ));
+      length: 2,
+      child: Scaffold(
+        body: TabBarView(children: [FormScreen(), DisplayScreen()]),
+        bottomNavigationBar: TabBar(
+            tabs: [Tab(text: 'บันทึกอุณหภูมิ'), Tab(text: 'รายชื่อนักเรียน')]),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    );
   }
 }
